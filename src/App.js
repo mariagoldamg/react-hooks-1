58f09cc5579030d24dba77634c3d262432fb7e65
mariagoldamg <mariamelnikova88@gmail.com>
2023-04-21 10:09:54 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react'
+//import { Component } from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState ('Maria');
+  const [age, setAge] = useState (35);
+
+  const updateData = () => {
+    setName("Andy");
+    setAge (44);
+  }
+  return(
+    <div>
+<h1>Hello {name}</h1>
+<h2>You are {age}</h2>
+<button onClick = {updateData}>Click Here</button>
+
+    </div>
+  )
+}
+
+
+export default App;
+
+
+
+/*class App extends Component{
+
+state ={
+  name: 'Maria'
+}
+
+changeName() {
+  this.setState(
+    {name: 'Andy'}
+  )
+}
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <h1>Hello {this.state.name}</h1>
+     <button onClick= {()=> this.changeName()}>Click Here</button>
     </div>
   );
 }
-
-export default App;
+}*/
